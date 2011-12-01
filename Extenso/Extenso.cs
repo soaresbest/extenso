567fce47788a4a4b2d162f2bf7de7d4c4f60cd37
Ivan Soares <soaresbest@gmail.com>
2011-12-01 14:08:07 -0200

    retiradas funcoes soltas e jogadas para as classes
    Dezena agora funciona para unidades

--- Extenso/Extenso.cs
+++ Extenso/Extenso.cs
@@ -6,138 +6,9 @@ namespace Extenso
     {
         public static string Converter(ulong numero)
         {
-            if (numero <= 9)
-            {
-                return ConverterUnidade(numero);
-            }
-            if (numero <= 19)
-            {
-                return ConverterDeDezAtehDezenove(numero);
-            }
-            if (numero >= 20 && numero <= 99)
-            {
-                string dezena = ConverterDezena(numero);
-                string unidade = ConverterUnidade(numero);
-
-                if (!string.IsNullOrEmpty(unidade))
-                    return string.Format("{0} e {1}", dezena, unidade);
-
-                return dezena;
-            }
-            throw new Exception("o número " + numero + " não pôde ser tratado.");
-        }
-
-        private static string ConverterUnidade(ulong numero)
-        {
-            var texto = numero.ToString();
-
-            var unidade = texto[texto.Length - 1];
-
-            switch (unidade)
-            {
-                case '0':
-                    {
-                        if (numero > 9)
-                            return "";
-
-                        return "zero";
-                    }
-                case '1':
-                    return "um";
-                case '2':
-                    return "dois";
-                case '3':
-                    return "três";
-                case '4':
-                    return "quatro";
-                case '5':
-                    return "cinco";
-                case '6':
-                    return "seis";
-                case '7':
-                    return "sete";
-                case '8':
-                    return "oito";
-                case '9':
-                    return "nove";
-                default:
-                    {
-                        throw new Exception(
-                        string.Format(
-                            "a unidade {0} do número {1} não está no intervalo [0-9].",
-                            unidade,
-                            numero));
-                    }
-            }
-        }
-
-        private static string ConverterDezena(ulong numero)
-        {
-            var texto = numero.ToString();
-
-            var dezena = texto[texto.Length - 2];
-
-            switch (dezena)
-            {
-                case '2':
-                    return "vinte";
-                case '3':
-                    return "trinta";
-                case '4':
-                    return "quarenta";
-                case '5':
-                    return "cinquenta";
-                case '6':
-                    return "sessenta";
-                case '7':
-                    return "setenta";
-                case '8':
-                    return "oitenta";
-                case '9':
-                    return "noventa";
-                default:
-                    {
-                        throw new Exception(
-                            string.Format(
-                                "a dezena {0} do número {1} não está no intervalo [2-9].",
-                                dezena,
-                                numero));
-                    }
-            }
-        }
-
-        private static string ConverterDeDezAtehDezenove(ulong numero)
-        {
-            switch (numero)
-            {
-                case 10:
-                    return "dez";
-                case 11:
-                    return "onze";
-                case 12:
-                    return "doze";
-                case 13:
-                    return "treze";
-                case 14:
-                    return "quatorze";
-                case 15:
-                    return "quinze";
-                case 16:
-                    return "dezesseis";
-                case 17:
-                    return "dezessete";
-                case 18:
-                    return "dezoito";
-                case 19:
-                    return "dezenove";
-                default:
-                    {
-                        throw new Exception(
-                            string.Format(
-                                "o número {0} não está no intervalo [10-19].",
-                                numero));
-                    }
-            }
+            string numeroTexto = numero.ToString();
+            var dezena = new Dezena(numeroTexto);
+            return dezena.ToString();
         }
     }
 
@@ -158,7 +29,7 @@ namespace Extenso
 
         private string DezenaUnidade
         {
-            get { return (AlgarismoDezena + AlgarismoUnidade).ToString(); }
+            get { return (string.Concat(AlgarismoDezena,AlgarismoUnidade)); }
         }
 
         public int ToInt()
@@ -168,16 +39,43 @@ namespace Extenso
 
         public Dezena(char algarismoDezena, char algarismoUnidade)
         {
+            Inicializar(algarismoDezena, algarismoUnidade);
+        }
+
+        private void Inicializar(char algarismoDezena, char algarismoUnidade)
+        {
             Unidade = new Unidade(algarismoUnidade);
 
             AlgarismoDezena = algarismoDezena;
             AlgarismoUnidade = algarismoUnidade;
         }
 
+        public Dezena(string texto)
+        {
+            char dezena='0';
+            char unidade;
+
+            if (texto.Length == 2)
+            {
+                dezena = texto[0];
+                unidade = texto[1];
+                Inicializar(dezena, unidade);
+            }
+            else
+            {
+                unidade = texto[0];
+                Inicializar(dezena, unidade);
+            }
+        }
+
         public override string ToString()
         {
             int numero = ToInt();
 
+            if (numero < 10)
+            {
+                return Unidade.ToString();
+            }
             if (numero >= 10 && numero <= 19)
             {
                 return DezDezenove();
